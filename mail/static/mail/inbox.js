72d1ef2d698c9a2a4f3bac38b23f35928071e66d
shumit taher <shumitt@gmail.com>
2024-05-15 17:25:29 +0600

    mail body queri selector

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,5 @@
+var mailBody;
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -8,6 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+
+  //declaring query selectors
+  mailBody = document.querySelector('#compose-body')
+
 });
 
 function compose_email() {
@@ -19,25 +26,83 @@ function compose_email() {
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  mailBody.value = '';
 }
 
 function load_mailbox(mailbox) {
 
-  let url = `/emails/${mailbox}`;
-
+  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-
+  
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-
+  
   //fetch query
+  
+  let url = `/emails/${mailbox}`;
+  ask_from_api(url);
+  
+}
 
+function ask_from_api(url) {
+  
   fetch(url)
   .then(response => response.json())
-  .then(emails=>console.log("fetch done: " + emails))
-  .catch(error => console.error('Error:', error));
+  .then((emails)=>{
+     
+    if ("error" in emails ){
+        populate_error(emails.error)
+      } else {
+        populate_emails(emails)
+      }
+      
+    })
+
+}
+
+function populate_error(mesg) {
+
+  document.querySelector('#emails-view').append(mesg)
+  
+}
+
+function populate_emails(emails) {
+
+  if (emails.length == 0) {
+    document.querySelector('#emails-view').append("No Emails Found")
+  }
+  
+
+
+}
+
+function email_submitted(event){
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: 'baz@example.com',
+        subject: 'Meeting time',
+        body: 'How about we meet tomorrow at 3pm?'
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+
+  return false;
+}
+
+function make_email_object(recipients, subject, body) {
+
+  return {
+    recipients: recipients,
+    subject: subject,
+    body: body
+  }
   
 }
